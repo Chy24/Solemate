aefe88d042a6314ad9e62dac0e0fe85087f19ed8
Chy <chymeng24@gmail.com>
2017-11-01 12:59:26 +1100

    Emoji for comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def emoji(message)
+    message.gsub(':smiley:', '😃').gsub(':hearteyes:', '😍').gsub(':kissyface:', '😘').gsub(':hearteyes:', '😍').gsub(':nerd:', '🤓').gsub(':winking:', '😉').gsub(':dog:', '🐶').gsub(':monkey:', '🐵').gsub(':sushi:', '🍣').gsub(':watermelon:', '🍉').gsub(':peach:', '🍑').gsub(':fire:', '🔥').gsub(':peace:', '✌️').gsub(':thumbsup:', '👍')
+   end
+
 end
