656ef894d0b160b4380e7a1134b42d03d9a15f02
Chy <chymeng24@gmail.com>
2017-11-02 15:22:18 +1100

    Added carousel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
 
   def emoji(message)
     message.gsub(':smiley:', '😃').gsub(':hearteyes:', '😍').gsub(':kissyface:', '😘').gsub(':hearteyes:', '😍').gsub(':nerd:', '🤓').gsub(':winking:', '😉').gsub(':dog:', '🐶').gsub(':monkey:', '🐵').gsub(':sushi:', '🍣').gsub(':watermelon:', '🍉').gsub(':peach:', '🍑').gsub(':fire:', '🔥').gsub(':peace:', '✌️').gsub(':thumbsup:', '👍')
-   end
+  end
 
 end
