ba5b1d57c51f2ea56595b9af4a136c0b34cf7d15
Chy <chymeng24@gmail.com>
2017-11-09 13:49:59 +1100

    Env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,11 +16,11 @@ CarrierWave.configure do |config|
   
   config.fog_credentials = {
     :provider               => 'AWS',                             # required
-    :aws_access_key_id      => ENV.fetch("S3_ACCESS_KEY_ID"),            # required
-    :aws_secret_access_key  => ENV.fetch("S3_SECRET_ACCESS_KEY"),     # required
-    :region                 => ENV.fetch("S3_REGION")                        # optional, defaults to 'us-east-1'
+    :aws_access_key_id      => ENV["S3_ACCESS_KEY_ID"],            # required
+    :aws_secret_access_key  => ENV["S3_SECRET_ACCESS_KEY"],     # required
+    :region                 => ENV["S3_REGION"]                       # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  =  ENV.fetch("S3_BUCKET")             # required
+  config.fog_directory  =  ENV["S3_BUCKET"]         # required
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   #config.fog_public     = false                                  # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
